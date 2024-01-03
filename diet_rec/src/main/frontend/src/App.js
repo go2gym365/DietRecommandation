21f9792075599d24992a2d1c753f02a60085f30b
Boiling SODA <go2gym365@gmail.com>
2024-01-03 13:42:07 +0000

    feat: Create a basic page and defalut setting

--- diet_rec/src/main/frontend/src/App.js
+++ diet_rec/src/main/frontend/src/App.js
@@ -1,28 +1,23 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { BrowserRouter as BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Board from "./pages/board/Board";
+import DietRec from "./pages/dietRec/DietRec";
+import Signin from "./pages/auth/signin/SignIn";
+import Signup from "./pages/auth/signup/SignUp";
 
 function App() {
-  const [message, setMessage] = useState([]);
-
-  useEffect(() => {
-    fetch("hello")
-      .then((response) => {
-        return response.json();
-      })
-      .then(function (data) {
-        setMessage(data);
-      });
-  }, []);
-
   return (
-    <div>
-      <div>시작</div>
-      <ul>
-        {message.map((text, index) => (
-          <li key={`${index}-${text}`}>{text}</li>
-        ))}
-      </ul>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/dietRec" element={<DietRec />} />
+          <Route path="/board" element={<Board />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
